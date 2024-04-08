30c2dfd1726650a7ed8e8851e4d1d56da80a10ee
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2024-04-09 00:56:42 +0200

    Initial Commit

---
+++ LIA1_Test_App/LIA1_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIA1_Test_AppApp.swift
+//  LIA1_Test_App
+//
+//  Created by Youssef Azroun on 2024-04-09.
+//
+
+import SwiftUI
+
+@main
+struct LIA1_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
